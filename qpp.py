d918697ec9ade7d298ce6221dd10be819e812553
Ahmed BAALI <baali.ahmed7@gmail.com>
2021-11-03 10:19:40 +0100

    The Button and events

--- qpp.py
+++ qpp.py
@@ -3,18 +3,34 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 
+class TestWindow(QMainWindow):
+    def __init__(self):
+        super(TestWindow, self).__init__()
+        self.setGeometry(400,400,200,200)
+        self.setWindowTitle("First app with PyQt5")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello World")
+        self.label.move(80,80)    
+
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me!")
+        self.b1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("The button is clicked!!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
 
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(400,400,200,200)
-    win.setWindowTitle("First app with PyQt5")
-
-
-    label = QtWidgets.QLabel(win)
-    label.setText("Hello World")
-    label.move(100,100)
-
+    win = TestWindow()
     win.show()
     sys.exit(app.exec_())
 
